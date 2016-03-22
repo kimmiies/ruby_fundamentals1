4ecbc9403e6a23bcbd05546a11e27d881259a0ac
Kimberly Oleiro <koleiro@gmail.com>
2016-03-22 14:38:00 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+# 1. Calculate a good tip for 55 dollar meal
+
+puts "I like tipping 20% so if the meal was $55 I would tip $#{55 * 0.2}"
+
+# 2. Adding a string and an integer
+# put quotations around 1 to make it a string and than add it to string
+
+puts "Add me to" + " 1"
+
+puts "add me as well " + 1.to_s
+
+# 3. Try outputting the result of 45628 multiplied by 7839 in a sentence
+
+puts "Multiply 45628 by 7839 will give you a big number like #{45628*7839}!"
+
+# 4. What is the value of this expression (10 < 20 && 30 < 20) || !(10 == 11)
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
